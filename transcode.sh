5d4109818e9edf9de75708018ef2302a8ed49f46
cade <git@cade.me>
2024-04-10 15:22:02 -0700

    speed up transcode script

--- transcode.sh
+++ transcode.sh
@@ -11,7 +11,7 @@ output="${input%.*}-transcoded.mp4"
 docker exec jelly /usr/lib/jellyfin-ffmpeg/ffmpeg \
   -i "$input" \
   -map 0:v -map 0:a \
-  -c:v libx264 -preset slow -crf 18 -profile:v high -level 4.1 -pix_fmt yuv420p \
+  -c:v libx264 -preset veryfast -crf 23 -profile:v high -level 4.1 -pix_fmt yuv420p \
   -c:a aac -b:a 192k -ar 48000 -ac 2 \
   -movflags +faststart \
   "$output"
